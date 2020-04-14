3bc1a0d4b0458f9b0ef26c35851666f06adadeeb
Call Vin <kanggara75@gmail.com>
2020-04-14 17:50:37 +0700

    mengubah index pada auih menhadi login.php

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -1,9 +1,19 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Auth extends CI_Controller {
+class Auth extends CI_Controller
+{
 	public function index()
 	{
-		$this->load->view('auth/index');
+		$this->load->view('template/auth_header');
+		$this->load->view('auth/login');
+		$this->load->view('template/auth_footer');
+	}
+
+	public function registration()
+	{
+		$this->load->view('template/auth_header');
+		$this->load->view('auth/registration');
+		$this->load->view('template/auth_footer');
 	}
 }
