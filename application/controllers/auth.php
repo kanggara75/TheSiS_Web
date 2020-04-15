8b5a4c5df19f34beca3097eaf6d904374167f44d
Call Vin <kanggara75@gmail.com>
2020-04-15 13:26:02 +0700

    finish registration feature

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -3,17 +3,57 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+
 	public function index()
 	{
+		$data['title']='TheSiS | Login';
+		$this->load->view('template/auth_header', $data);
 		$this->load->view('template/auth_header');
 		$this->load->view('auth/login');
+		
 		$this->load->view('template/auth_footer');
 	}
 
 	public function registration()
 	{
-		$this->load->view('template/auth_header');
-		$this->load->view('auth/registration');
-		$this->load->view('template/auth_footer');
+		$this->form_validation->set_rules('name', 'Name', 'required|trim');
+		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+			'is_unique' => 'This Email already registered!'
+		]);
+		$this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', 
+		[
+			'matches' => 'Password not match',
+			'min_length' => 'Password too short'
+		]);
+		$this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+
+		if($this->form_validation->run() == false){
+			$data['title']='TheSiS | Registration';
+			$this->load->view('template/auth_header', $data);
+			$this->load->view('auth/registration');
+			$this->load->view('template/auth_footer');
+		}
+		else {
+			$data = [
+				'name' => $this->input->post('name'),
+				'email' => $this->input->post('email'),
+				'image' => 'default.jpg',
+				'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+				'role_id' => 2,
+				'is_active' => 1,
+				'date_created' => time()
+			];
+
+			$this->db->insert('user', $data);
+			$this->session->set_flashdata('messege', '<div class="alert alert-success" role="alert">Account Successfully Registered! </div>');
+			redirect('auth');
+		}
+		
 	}
 }
--- application/views/landing/index.php
+++ application/views/landing/index.php
@@ -23,7 +23,7 @@
   <!-- NavBar -->
   <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
-      <a class="navbar-brand" href="#">TheSiS</a>
+      <a class="navbar-brand" href="<?= base_url() ?>">TheSiS</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
         aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -33,7 +33,7 @@
           <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
           <a class="nav-item nav-link" href="#">Features</a>
           <a class="nav-item nav-link" href="#">About</a>
-          <a class="nav-item tombol btn btn-primary" href="#">Join Us</a>
+          <a class="nav-item tombol btn btn-primary" href="<?= base_url('auth/registration') ?>">Join Us</a>
         </div>
       </div>
     </div>
