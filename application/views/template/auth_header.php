b235691a59680116f9b642b159f6845716220c80
Call Vin <kanggara75@gmail.com>
2020-04-15 13:23:50 +0700

    automatic Title bar

--- application/views/template/auth_header.php
+++ application/views/template/auth_header.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>TheSiS - Login</title>
+    <title><?= $title;?></title>
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('assets/') ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -21,5 +21,4 @@
     <link rel="icon" href="<?= base_url('assets/') ?>img/icon.png" />
 
 </head>
-
 <body class="bg-gradient-primary">
