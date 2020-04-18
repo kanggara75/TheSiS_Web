ebcce12d6b79505f44609220def7243387dd508a
Call Vin <kanggara75@gmail.com>
2020-04-18 15:31:30 +0700

    Comit lah

--- application/controllers/Landing.php
+++ application/controllers/Landing.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Landing extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('landing');
+		$this->load->view('landing/index');
 	}
 }
