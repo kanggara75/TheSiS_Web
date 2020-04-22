982436015a267408a2670c399ab9f873f981cf89
Call Vin <kanggara75@gmail.com>
2020-04-22 07:19:56 +0700

    Komentar untuk hosting

--- application/config/config.php
+++ application/config/config.php
@@ -22,6 +22,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | If you need to allow multiple domains, remember that this file is still
 | a PHP script and you can easily do that on your own.
 |
+| $config['base_url'] = 'https://thesis.kanggara.net/';
 */
 $config['base_url'] = 'https://localhost/TheSiS_Web/';
 
