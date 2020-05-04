894bbb17e2ed1079d8879427ab8b6edf379a3ecc
Call Vin <kanggara75@gmail.com>
2020-05-04 14:44:59 +0700

    Add Feature | Menu Management

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -24,14 +24,14 @@
   $menu = $this->db->query($queryMenu)->result_array();
   ?>
 
-  <!-- Looping Menu -->
-  <?php foreach($menu as $m) : ?>
-  <div class="sidebar-heading">
-    <?=$m['menu'];?>
-  </div>
-  
-  <!-- Sub Menu Looping -->
-  <?php
+	<!-- Looping Menu -->
+	<?php foreach($menu as $m) : ?>
+	<div class="sidebar-heading">
+		<?=$m['menu'];?>
+	</div>
+
+	<!-- Sub Menu Looping -->
+	<?php
     $menuId = $m['id'];
     $querySubMenu = "SELECT * FROM `user_sub_menu` 
                       WHERE `menu_id` = $menuId
@@ -39,80 +39,81 @@
     ";
     $subMenu = $this->db->query($querySubMenu)->result_array();
   ?>
-  <?php foreach ($subMenu as $sm) : ?>
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-      <a class="nav-link" href="<?= base_url($sm['url']); ?>">
-      <i class="<?=$sm['icon'];?>"></i>
-      <span><?=$sm['title'];?></span></a>
-    </li>
-  <?php endforeach;?>
-  <!-- Divider -->
-  <hr class="sidebar-divider">
+	<?php foreach ($subMenu as $sm) : ?>
+	<!-- Nav Item - Dashboard -->
+	<?php if($title == $sm['title']): ?>
+	<li class="nav-item active">
+	<?php else: ?>
+		<li class="nav-item">
+	<?php endif; ?>
+		<a class="nav-link" href="<?= base_url($sm['url']); ?>">
+			<i class="<?=$sm['icon'];?>"></i>
+			<span><?=$sm['title'];?></span></a>
+	</li>
+	<?php endforeach;?>
+	<!-- Divider -->
+	<hr class="sidebar-divider">
 
-  <?php endforeach;?>
+	<?php endforeach;?>
 
- 
+	<!-- Heading -->
+	<div class="sidebar-heading">
+		Interface
+	</div>
 
-  
-  <!-- Heading -->
-  <div class="sidebar-heading">
-    Interface
-  </div>
+	<!-- Nav Item - Pages Collapse Menu -->
+	<li class="nav-item">
+		<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
+			aria-controls="collapseTwo">
+			<i class="fas fa-fw fa-cog"></i>
+			<span>Components</span>
+		</a>
+		<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+			<div class="bg-white py-2 collapse-inner rounded">
+				<h6 class="collapse-header">Custom Components:</h6>
+				<a class="collapse-item" href="buttons.html">Buttons</a>
+				<a class="collapse-item" href="cards.html">Cards</a>
+			</div>
+		</div>
+	</li>
 
-  <!-- Nav Item - Pages Collapse Menu -->
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-    aria-expanded="true" aria-controls="collapseTwo">
-    <i class="fas fa-fw fa-cog"></i>
-    <span>Components</span>
-  </a>
-  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-    <div class="bg-white py-2 collapse-inner rounded">
-      <h6 class="collapse-header">Custom Components:</h6>
-      <a class="collapse-item" href="buttons.html">Buttons</a>
-      <a class="collapse-item" href="cards.html">Cards</a>
-    </div>
-  </div>
-  </li>
+	<!-- Nav Item - Utilities Collapse Menu -->
+	<li class="nav-item">
+		<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true"
+			aria-controls="collapseUtilities">
+			<i class="fas fa-fw fa-wrench"></i>
+			<span>Utilities</span>
+		</a>
+		<div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+			<div class="bg-white py-2 collapse-inner rounded">
+				<h6 class="collapse-header">Custom Utilities:</h6>
+				<a class="collapse-item" href="utilities-color.html">Colors</a>
+				<a class="collapse-item" href="utilities-border.html">Borders</a>
+				<a class="collapse-item" href="utilities-animation.html">Animations</a>
+				<a class="collapse-item" href="utilities-other.html">Other</a>
+			</div>
+		</div>
+	</li>
 
-  <!-- Nav Item - Utilities Collapse Menu -->
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-      <i class="fas fa-fw fa-wrench"></i>
-      <span>Utilities</span>
-    </a>
-    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-    data-parent="#accordionSidebar">
-    <div class="bg-white py-2 collapse-inner rounded">
-      <h6 class="collapse-header">Custom Utilities:</h6>
-      <a class="collapse-item" href="utilities-color.html">Colors</a>
-      <a class="collapse-item" href="utilities-border.html">Borders</a>
-      <a class="collapse-item" href="utilities-animation.html">Animations</a>
-      <a class="collapse-item" href="utilities-other.html">Other</a>
-    </div>
-  </div>
-</li>
-      
-  <!-- Divider -->
-  <hr class="sidebar-divider">
-  <div class="sidebar-heading">
-    Main Menu
-  </div>
+	<!-- Divider -->
+	<hr class="sidebar-divider">
+	<div class="sidebar-heading">
+		Main Menu
+	</div>
 
-  <!-- Nav Item - Dashboard -->
-  <li class="nav-item">
-    <a class="nav-link" href="<?= base_url('auth/logout'); ?>">
-    <i class="fas fa-fw fa-sign-out-alt"></i>
-    <span>Logout</span></a>
-  </li>
-  
-  <!-- Divider -->
-  <hr class="sidebar-divider d-none d-md-block">
+	<!-- Nav Item - Dashboard -->
+	<li class="nav-item">
+		<a class="nav-link" href="<?= base_url('auth/logout'); ?>">
+			<i class="fas fa-fw fa-sign-out-alt"></i>
+			<span>Logout</span></a>
+	</li>
 
-  <!-- Sidebar Toggler (Sidebar) -->
-  <div class="text-center d-none d-md-inline">
-    <button class="rounded-circle border-0" id="sidebarToggle"></button>
-  </div>
+	<!-- Divider -->
+	<hr class="sidebar-divider d-none d-md-block">
+
+	<!-- Sidebar Toggler (Sidebar) -->
+	<div class="text-center d-none d-md-inline">
+		<button class="rounded-circle border-0" id="sidebarToggle"></button>
+	</div>
 </ul>
 <!-- End of Sidebar -->
