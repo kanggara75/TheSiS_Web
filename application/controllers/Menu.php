b5504588b9c6aed4286ecca5803feca0fc3f0863
Call Vin <kanggara75@gmail.com>
2020-05-04 15:45:35 +0700

    Add Feature | Sub Menu Management

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -23,6 +23,42 @@ class Menu extends CI_Controller
       $this->session->set_flashdata('messege', '<div class="alert alert-success" role="alert">New Menu added!</div>');
 			redirect('menu');
     }    
+  }  
+  
+  public function submenu()
+  {
+    $data['title'] = 'Sub Menu Management';
+    $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+    $this->load->model('Menu_model', 'menu');
+    $data['menu'] = $this->db->get('user_menu')->result_array();
+
+    $data['subMenu'] = $this->menu->getSubMenu();
+
+    $this->form_validation->set_rules('title', 'Title', 'required');
+    $this->form_validation->set_rules('menu_id', 'Menu', 'required');
+    $this->form_validation->set_rules('url', 'URL', 'required');
+    $this->form_validation->set_rules('icon', 'Icon', 'required');
+
+    if ($this->form_validation->run() == false)
+    {
+      $this->load->view('template/header', $data);
+      $this->load->view('template/sidebar', $data);
+      $this->load->view('template/topbar', $data);
+      $this->load->view('menu/submenu', $data);
+      $this->load->view('template/footer');
+    }else{
+      $data = [
+        'title' => $this->input->post('title'),
+        'menu_id' => $this->input->post('menu_id'),
+        'url' => $this->input->post('url'),
+        'icon' => $this->input->post('icon'),
+        'is_active' => $this->input->post('is_active')
+      ];
+      $this->db->insert('user_sub_menu', $data);
+      $this->session->set_flashdata('messege', '<div class="alert alert-success" role="alert">New Sub Menu added!</div>');
+      redirect('menu/submenu');
+    }
+    
   }
 }
 
