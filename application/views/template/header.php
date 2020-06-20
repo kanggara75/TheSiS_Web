86652269398a2a6e4634038e6261dd187098b006
Call Vin <kanggara75@gmail.com>
2020-06-20 23:06:24 +0700

    menambahkan css untuk switch

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -13,6 +13,8 @@
 	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 	<!-- Custom styles for this template-->
 	<link href="<?= base_url('assets/') ?>css/sb-admin-2.min.css" rel="stylesheet">
+	<!-- My Css For Switch-->
+	<link href="<?= base_url('assets/') ?>css/switch.css" rel="stylesheet">
 	<!-- Google Adsense -->
 	<script data-ad-client="ca-pub-7125226131467905" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 	<!-- My Icon -->
