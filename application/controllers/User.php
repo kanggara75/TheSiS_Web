8c0c507806fc2a9629660f65b35d1054f8505ae6
Call Vin <kanggara75@gmail.com>
2020-06-21 21:36:41 +0700

    add security (must Login first) and delete error mesege

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,14 +3,22 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class User extends CI_Controller
 {
-    public function index()
+  public function __construct()
+  {
+    parent::__construct();
+    if(!$this->session->userdata('email')){
+      redirect('auth');
+    }
+  }
+  
+  public function index()
 	{
-        $data['title'] = 'My Profile';
-        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-        $this->load->view('template/header', $data);
-        $this->load->view('template/sidebar', $data);
-        $this->load->view('template/topbar', $data);
-        $this->load->view('user/index', $data);
-        $this->load->view('template/footer');
+    $data['title'] = 'My Profile';
+    $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+    $this->load->view('template/header', $data);
+    $this->load->view('template/sidebar', $data);
+    $this->load->view('template/topbar', $data);
+    $this->load->view('user/index', $data);
+    $this->load->view('template/footer');
 	}
 }
