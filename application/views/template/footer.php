b6d997e06d825c9a1d35b52550b910a3fee66fb7
Call Vin <kanggara75@gmail.com>
2020-06-21 21:28:14 +0700

    perbaiki url script control panel

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,10 +1,10 @@
 <!-- Footer -->
 <footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-        <div class="copyright text-center my-auto">
-            <span class=”copy-left”>Copyright &copy; KAnggara75 <?= date('F Y')?></span>
-        </div>
-    </div>
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span class=”copy-left”>Copyright &copy; KAnggara75 <?= date('F Y')?></span>
+		</div>
+	</div>
 </footer>
 <!-- End of Footer -->
 </div>
@@ -14,26 +14,27 @@
 
 <!-- Scroll to Top Button-->
 <a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
+	<i class="fas fa-angle-up"></i>
 </a>
 
 <!-- Logout Modal-->
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="<?= base_url('auth/logout');?>">Logout</a>
-            </div>
-        </div>
-    </div>
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+	aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+				<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">×</span>
+				</button>
+			</div>
+			<div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+			<div class="modal-footer">
+				<button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+				<a class="btn btn-primary" href="<?= base_url('auth/logout');?>">Logout</a>
+			</div>
+		</div>
+	</div>
 </div>
 
 <!-- Bootstrap core JavaScript-->
@@ -55,15 +56,14 @@
 	function updateOutput(element) {
 		var xhr = new XMLHttpRequest();
 		if (element.checked) {
-      xhr.open("GET", "updateKontrol?id=" + element.id + "&state=1", true);
+			xhr.open("GET", "/TheSiS_Web/admin/updateKontrol?id=" + element.id + "&state=1", true);
 		} else {
-			xhr.open("GET", "updateKontrol?id=" + element.id + "&state=0", true);
+			xhr.open("GET", "/TheSiS_Web/admin/updateKontrol?id=" + element.id + "&state=0", true);
 		}
 		xhr.send();
-		
 	}
 </script>
-<?php elseif($title == "My Profile"): ?> 
+<?php elseif($title == "My Profile"): ?>
 <!-- Page level plugins -->
 <script src="<?= base_url('assets/') ?>vendor/chart.js/Chart.min.js"></script>
 <!-- Page level custom scripts -->
@@ -72,9 +72,9 @@
 	function updateOutput(element) {
 		var xhr = new XMLHttpRequest();
 		if (element.checked) {
-      xhr.open("GET", "updateKontrol?id=" + element.id + "&state=1", true);
+			xhr.open("GET", "/TheSiS_Web/user/updateKontrol?id=" + element.id + "&state=1", true);
 		} else {
-			xhr.open("GET", "updateKontrol?id=" + element.id + "&state=0", true);
+			xhr.open("GET", "/TheSiS_Web/user/updateKontrol?id=" + element.id + "&state=0", true);
 		}
 		xhr.send();
 	}
