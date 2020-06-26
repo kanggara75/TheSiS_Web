96a180d6d4ad7a0bc481d836c1397ac7c26d4929
Call Vin <kanggara75@gmail.com>
2020-06-27 01:10:45 +0700

    Menyelesaikan Edit Profil

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -62,6 +62,7 @@
 		}
 		xhr.send();
 	}
+
 </script>
 <?php elseif($title == "My Profile"): ?>
 <!-- Page level plugins -->
@@ -78,29 +79,33 @@
 		}
 		xhr.send();
 	}
+
 </script>
 <?php else: ?>
 <?php endif; ?>
 <!-- AJAX change Access -->
 <script>
-$('.form-check-input').on('click', function() {
-	const menuId = $(this).data('menu');
-	const roleId = $(this).data('role');
+	$('.custom-file-input').on('change', function () {
+		let fileName = $(this).val().split('\\').pop();
+		$(this).next('.custom-file-label').addClass("selected").html(fileName);
+	});
 
-	$.ajax({
-		url: "<?= base_url('admin/changeaccess'); ?>",
-		type: 'post',
-		data: {
-			menuId: menuId,
-			roleId: roleId
-		},
-		success: function()
-		{
-			document.location.href = "<?= base_url('admin/roleAccess/'); ?>" + roleId;
-		}
-	})
-	
-})
+	$('.form-check-input').on('click', function () {
+		const menuId = $(this).data('menu');
+		const roleId = $(this).data('role');
+		$.ajax({
+			url: "<?= base_url('admin/changeaccess'); ?>",
+			type: 'post',
+			data: {
+				menuId: menuId,
+				roleId: roleId
+			},
+			success: function () {
+				document.location.href = "<?= base_url('admin/roleAccess/'); ?>" + roleId;
+			}
+		})
+	});
+
 </script>
 </body>
 
--- application/views/user/index.php
+++ application/views/user/index.php
@@ -3,7 +3,12 @@
 
 	<!-- Page Heading -->
 	<h1 class="h3 mb-4 text-gray-800"><?= $user['name']?></h1>
-	<div class="card mb-3" style="max-width: 540px;">
+	<div class="row">
+		<div class="col-lg-8">
+			<?= $this->session->flashdata('messege'); ?>
+		</div>
+	</div>
+	<div class="card mb-3 col-lg-8">
 		<div class="row no-gutters">
 			<div class="col-md-4">
 				<img src="<?= base_url('assets/img/profile/') . $user['image'];?>" class="card-img">
