6933ef3971cdf592972573acaaf9470acbb3a79d
Call Vin <kanggara75@gmail.com>
2020-06-30 05:12:21 +0700

    Menambahkan fitur Lupa Password

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -130,7 +130,10 @@ class Auth extends CI_Controller
 		if($type == 'verify'){
 			$this->email->subject('Account Activation');
 			$this->email->message('Click this link to activate your TheSiS Account: <a href="' . base_url() . 'auth/verify?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '">Here</a>');
-		} 
+		} elseif ($type == 'forgot') {
+			$this->email->subject('Reset Password');
+			$this->email->message('Click this link to reset your TheSiS Account Password: <a href="' . base_url() . 'auth/reset?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '">Here</a>');
+		}
 		if($this->email->send()){
 			return true;
 		} else {
@@ -184,4 +187,92 @@ class Auth extends CI_Controller
 	{
 		$this->load->view('errors/html/error_403');
 	}
+
+	public function forgot()
+	{
+		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+		if($this->form_validation->run() == false){
+			$data['title']='Forgot Password';
+			$this->load->view('template/auth_header', $data);
+			$this->load->view('auth/forgot');
+			$this->load->view('template/auth_footer');
+		} else {
+			$email = $this->input->post('email');
+			$cek_user_token = $this->db->get_where('user_token', ['email' => $email])->row_array();
+			if ($cek_user_token) {
+				$this->db->delete('user_token', ['email' => $email]);
+				$user = $this->db->get_where('user', ['email' => $email, 'is_active' => 1])->row_array();
+				if($user){
+					$token = base64_encode(random_bytes(32));
+					$user_token = [
+												'email' => $email,
+												'token' => $token,
+												'date' => time()
+					];
+
+					$this->db->insert('user_token', $user_token); 
+					$this->_sendEmail($token, 'forgot');
+					$this->session->set_flashdata('messege', '<div class="alert alert-success" role="alert">Please Check Your Email to reset your password</div>');
+					redirect('auth/forgot');
+					} else {
+						$this->session->set_flashdata('messege', '<div class="alert alert-danger" role="alert">Email Not Found or Not Activated!</div>');
+						redirect('auth/forgot');
+				}
+			}
+		}
+	}
+
+	public function reset()
+	{
+		$email = $this->input->get('email');
+		$token = $this->input->get('token');
+		$user = $this->db->get_where('user', ['email' => $email])->row_array();
+		if($user){
+			$user_token = $this->db->get_where('user_token', ['token' => $token])->row_array();
+			if($user_token){
+				if(time() - $user_token['date'] < (60*60*24)){
+					$this->session->set_userdata('reset_pass', $email);
+					$this->changePass();
+				} else {
+					$this->db->delete('user', ['email' => $email]);
+					$this->db->delete('user_token', ['email' => $email]);
+					$this->session->set_flashdata('messege', '<div class="alert alert-danger" role="alert">Token Expiert!</div>');
+					redirect('auth');
+				}
+			} else {
+				$this->session->set_flashdata('messege', '<div class="alert alert-danger" role="alert">Token Invalid!</div>');
+				redirect('auth');
+			}
+		} else {
+			$this->session->set_flashdata('messege', '<div class="alert alert-danger" role="alert">Email Not Found or Not Activated!</div>');
+			redirect('auth');
+		}
+	}
+
+	public function changePass()
+	{
+		if(!$this->session->userdata('reset_pass')){
+			redirect('auth');
+		}
+		$this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[6]|matches[password2]');
+    $this->form_validation->set_rules('password2', 'Confirm New Password', 'required|trim|min_length[6]|matches[password1]');
+		if($this->form_validation->run() == false){
+			$data['title']='Reset Password';
+			$this->load->view('template/auth_header', $data);
+			$this->load->view('auth/cp');
+			$this->load->view('template/auth_footer');
+		} else {
+			$password1 = $this->input->post('password1');
+			$email = $this->session->userdata('reset_pass');
+			$password = password_hash($password1, PASSWORD_DEFAULT);
+			$this->db->set('password', $password);
+			$this->db->where('email', $email);
+			$this->db->update('user');
+			$this->db->delete('user_token', ['email' => $email]);
+			$this->session->unset_userdata('reset_pass');
+			$this->session->set_flashdata('messege', '<div class="alert alert-success" role="alert">Password Has been Change!</div>');
+			redirect('auth');
+		}
+	}
+
 }
