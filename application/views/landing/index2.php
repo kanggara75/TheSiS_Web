9e8557db0d41c39e1d2e1bed87c9c74b6d1774c1
Call Vin <kanggara75@gmail.com>
2020-12-08 00:27:41 +0700

    Menyelesaikan Landing page V2

--- application/views/landing/index2.php
+++ application/views/landing/index2.php
@@ -5,16 +5,12 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   <!-- My CSS -->
   <link rel="stylesheet" href="<?= base_url('assets/') ?>css/landing.css" />
   <link rel="stylesheet" href="<?= base_url('assets/') ?>css/landingresponsive.css" />
-
-
   <!-- My Icon -->
   <link rel="icon" href="<?= base_url('assets/') ?>img/icon.png" />
-
   <!-- Title -->
   <title>TheSiS | <?= $title; ?></title>
 </head>
@@ -23,64 +19,64 @@
   <header>
     <div class="container">
       <div class="header-left">
-        <img class="logo" src="https://prog-8.com/images/html/advanced/main_logo.png">
+        <img class="logo" src="<?= base_url('assets/') ?>img/logo.png">
       </div>
       <!-- Tambahkan menu ikon dibaris bawah -->
       <span class="fa fa-bars menu-icon"></span>
       <div class="header-right">
-        <a href="#">Pelajaran</a>
-        <a href="#">Daftar</a>
-        <a href="#" class="login">Log in</a>
+        <a href="<?= base_url('simulation') ?>">Simulasi</a>
+        <a href="<?= base_url('auth/registration') ?>">Daftar</a>
+        <a href="<?= base_url('auth') ?>" class="login">Masuk</a>
       </div>
     </div>
   </header>
   <div class="top-wrapper">
     <div class="container">
-      <h1>BELAJAR CODING.</h1>
-      <h1>BELAJAR MENJADI LEBIH KREATIF.</h1>
-      <p>Progate adalah platform online untuk belajar pemrograman.</p>
-      <p>Kami menawarkan lingkungan pemrograman yang lengkap untuk mempermudah Anda memulai.</p>
+      <h1>SMART EARLY WARNING SYSTEM.</h1>
+      <h1>PENGAMANAN LANJUTAN PADA KENDARAAN ANDA.</h1>
+      <p>TheSiS merupakan sebuah sistem keamanan pada kendaraan berbasis Internet of Think.</p>
+      <p>Sistem ini menawarkan keamanan dan pemantauan tambahan pada kendaraan anda, juga dapat dijadikan saklar untuk mematikan kendaraan dari jarak jauh.</p>
       <div class="btn-wrapper">
-        <a href="#" class="btn signup">Daftar dengan Email</a>
+        <a href="#" class="btn signup">Simulasikan</a>
         <p>atau</p>
-        <a href="#" class="btn facebook"><span class="fa fa-facebook"></span>Daftar dengan Facebook</a>
-        <a href="#" class="btn twitter"><span class="fa fa-twitter"></span>Daftar dengan Twitter</a>
+        <a href="#" class="btn facebook">Daftar</a>
+        <a href="#" class="btn twitter">Masuk</a>
       </div>
     </div>
   </div>
   <div class="lesson-wrapper">
     <div class="container">
       <div class="heading">
-        <h2>Cari tau dari mana anda mau memulai!</h2>
+        <h2>Cari tau di mana kendaraan anda berada!</h2>
       </div>
       <div class="lessons">
         <div class="lesson">
           <div class="lesson-icon">
             <img src="https://prog-8.com/images/html/advanced/html.png">
-            <p>HTML & CSS</p>
+            <p>Tracking</p>
           </div>
-          <p class="txt-contents">Dua bahasa yang digunakan untuk membangun struktur dan design tambilan website anda.</p>
+          <p class="txt-contents">Lacak keberadaan kendaraan anda melalui koordinat yang akan diarahkan dengan Google Map.</p>
         </div>
         <div class="lesson">
           <div class="lesson-icon">
             <img src="https://prog-8.com/images/html/advanced/jQuery.png">
-            <p>jQuery</p>
+            <p>Monitoring</p>
           </div>
-          <p class="txt-contents">JavaScript Library yang cepat, kaya akan fitur dan mudah digunakan untuk menangani animasi dan permintaan Ajax.</p>
+          <p class="txt-contents">Pantau kondisi dan keadaan kendaraan anda hanya dengan internet.</p>
         </div>
         <div class="lesson">
           <div class="lesson-icon">
             <img src="https://prog-8.com/images/html/advanced/ruby.png">
-            <p>Ruby</p>
+            <p>Control</p>
           </div>
-          <p class="txt-contents">Bahasa dinamis, serba guna yang sederhana dan produktif. Ruby sering digunakan untuk membuat aplikasi web yang responsive.</p>
+          <p class="txt-contents">Menonaktifkan kendaraan secara langsung dari jarak jauh hanya dengan koneksi internet.</p>
         </div>
         <div class="lesson">
           <div class="lesson-icon">
             <img src="https://prog-8.com/images/html/advanced/php.png">
-            <p>PHP</p>
+            <p>Live Report</p>
           </div>
-          <p class="txt-contents">Bahasa scripting "open-source" yang dapat disematkan ke dalam HTML, dan cocok untuk pengembangan web.</p>
+          <p class="txt-contents">Dapatkan Notifikasi secara langsung jika terdapat perubahan/pergerakan dari kendaraan anda secara langsung.</p>
         </div>
         <div class="clear"></div>
       </div>
@@ -89,16 +85,18 @@
   <div class="message-wrapper">
     <div class="container">
       <div class="heading">
-        <h2>Apakah anda siap untuk menjadi programer yang handal?</h2>
+        <h2>Apakah Anda siap untuk bergabung menjadi pengembang sistem ini?</h2>
         <h3>Ayo belajar coding, ayo belajar menjadi lebih kreatif!</h3>
       </div>
-      <span class="btn message">Mulai belajar</span>
+      <a href="<?= base_url('about') ?>" target="_blank">
+        <span class=" btn message">About Us</span>
+      </a>
     </div>
   </div>
   <footer>
     <div class="container">
-      <img src="https://prog-8.com/images/html/advanced/footer_logo.png">
-      <p>Learn to code, learn to be creative.</p>
+      <img src="<?= base_url('assets/') ?>img/footer_logo.png">
+      <p>Tracking Secure System.</p>
     </div>
   </footer>
 </body>
