dd43e00139c6165a2b7d910f44daea43146e86bb
Call Vin <kanggara75@gmail.com>
2021-01-21 04:37:14 +0700

    Complicated response user data from database

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -11,27 +11,29 @@ class Api extends CI_Controller
       $password = $_POST['password'];
 
       $user = $this->db->get_where('user', ['email' => $email])->row_array();
+      $response = [
+        'name' => $user['name'],
+        'email' => $user['email'],
+        'image' => $user['image'],
+        'password' => $user['password'],
+        'value' => $user['role_id'],
+        'is_active' => $user['is_active'],
+        'date_created' => $user['date_created'],
+      ];
 
       if ($user) {
         if ($user['is_active'] == 1) {
           if (password_verify($password, $user['password'])) {
-            $data = [
-              'email' => $user['email'],
-              'role_id' => $user['role_id']
-            ];
-            $this->session->set_userdata($data);
             if ($user['role_id'] == 1) {
-              $response['value'] = 1;
               $response['messege'] = "Login Admin Berhasil";
               echo json_encode($response);
             } else {
-              $response['value'] = 2;
               $response['messege'] = "Login Member Berhasil";
               echo json_encode($response);
             }
           } else {
             $response['value'] = 0;
-            $response['messege'] = "Login Gagal";
+            $response['messege'] = "Username or Password is Wrong!";
             echo json_encode($response);
           }
         } else {
