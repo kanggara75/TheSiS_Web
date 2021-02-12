bbf8e98dd1f2806f11a641610c653a1d82ca8b8b
Call Vin <kanggara75@gmail.com>
2021-02-13 05:45:19 +0700

    change admin page [unfinish]

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -20,7 +20,7 @@ class Admin extends CI_Controller
     $this->load->view('template/header', $data);
     $this->load->view('template/sidebar', $data);
     $this->load->view('template/topbar', $data);
-    $this->load->view('admin/index', $data);
+    $this->load->view('admin/admin', $data);
     $this->load->view('template/footer', $data);
   }
 
