b1ffcc3de09f0a692026a263231738b54121873d
Call Vin <kanggara75@gmail.com>
2021-02-13 05:11:54 +0700

    add coment

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -18,7 +18,7 @@
 				</div>
 			</div>
 
-			<!-- Area Chart -->
+			<!-- Map List -->
 			<div class="card shadow mb-4">
 				<!-- Card Header - Dropdown -->
 				<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
